6551b05d6099827197a0b9f19c0e978b38913bc7
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-28 14:21:21 +0000

    refactor(IndexedDB): enhance class with improved type handling, additional methods for store management, and streamlined data handling

--- src/lib/indexeddb.class.ts
+++ src/lib/indexeddb.class.ts
@@ -1,29 +1,46 @@
 // Class.
-import { IDBConnection } from './idb-connection.class';
 import { IDBData } from './idb-data.class';
 import { IDBQuery } from './idb-query.class';
-import { IDBStore } from './idb-store.class';
-
-// Interface.
-import { IDBConfig } from '../interface/idb-config.interface';
-
-// Type.
-import { IDBStoreParameters } from '../type/idb-store-parameters.type';
-
+// Interface & Type.
+import {
+  IDBConfig,
+  IDBOpenStoreResult,
+  IDBOpenStoresResult,
+  IDBQueryTransactionOptions,
+  IDBSchema,
+  IDBStoreParameters,
+  IDBStoreQuery,
+  IDBStoresParameters,
+} from '@typedly/indexeddb';
 /**
- * 
+ * @description
+ * @export
+ * @class IndexedDB
+ * @template {IDBSchema} Schema 
+ * @template {IDBData<Schema, DBName, StoreNames, Version>} DataInstance 
+ * @template {string} [DBName=DataInstance extends IDBData<Schema, infer N, any, any> ? N : string] 
+ * @template {keyof Schema & string} [StoreNames=DataInstance extends IDBData<Schema, any, infer S, any> ? S : keyof Schema & string] 
+ * @template {number} [Version=DataInstance extends IDBData<Schema, any, any, infer V> ? V : number] 
  */
 export class IndexedDB<
-  StoreSchema extends object,
-  Name extends string = string,
-  StoreNames extends keyof StoreSchema = keyof StoreSchema,
-  Version extends number = number
+  DataInstance extends IDBData<Schema, DBName, StoreNames, Version>,
+  Schema extends IDBSchema,
+  DBName extends string = DataInstance extends IDBData<Schema, infer N, any, any> ? N : string,
+  StoreNames extends keyof Schema & string = DataInstance extends IDBData<Schema, any, infer S, any> ? S : keyof Schema & string,
+  Version extends number = DataInstance extends IDBData<Schema, any, any, infer V> ? V : number,
 > {
-  /**
-   * 
-   * @param config 
-   * @returns 
-   */
+  public static create<Schema extends IDBSchema, T extends boolean = false>() {
+    return <
+      DataInstance extends IDBData<Schema, DBName, StoreNames, Version>,
+      DBName extends string = DataInstance extends IDBData<Schema, infer N, any, any> ? N : string,
+      StoreNames extends keyof Schema & string = DataInstance extends IDBData<Schema, any, infer S, any> ? S : keyof Schema & string,
+      Version extends number = DataInstance extends IDBData<Schema, any, any, infer V> ? V : number,
+    >(data: T extends true ? DataInstance : { name: DBName; parameters: IDBStoresParameters<StoreNames>; version: Version }) => 
+        new IndexedDB<DataInstance, Schema, DBName, StoreNames, Version>(
+      data as any
+    );
+  }
+
   public static config: <
     Name extends string,
     StoreNames extends string,
@@ -31,90 +48,115 @@ export class IndexedDB<
   >(config: IDBConfig<Name, StoreNames, Version>) => IDBConfig<Name, StoreNames, Version> =
     <
       Name extends string,
-      StoreName extends string,
+      DBName extends string,
       Version extends number = number
-    >(config: IDBConfig<Name, StoreName, Version>) => config;
+    >(config: IDBConfig<Name, DBName, Version>) => config;
 
-  /**
-   * 
-   * @param store 
-   * @returns 
-   */
-  public static store: <StoreNames extends string>(store: IDBStoreParameters<StoreNames>) => IDBStoreParameters<StoreNames> =
-    <StoreNames extends string>(store: IDBStoreParameters<StoreNames>) => store;
 
-  /**
-   * 
-   */
+  public static store: (store: IDBStoreParameters) => IDBStoreParameters =
+    (store: IDBStoreParameters) => store;
+
   public get connection() {
-    return this.#query.connection;
+    return this.#data.connection;
   }
 
-  /**
-   * 
-   */
-  public get store(): IDBStore<StoreSchema, Name, StoreNames, Version> {
-    return this.#query.objectStore;
+  public get data() {
+    return this.#data;
+  }
+  
+  #data!: DataInstance;
+
+  constructor(settings: {
+    name: DBName,
+    parameters: IDBStoresParameters<StoreNames>,
+    version: Version
+  })
+  constructor(data: DataInstance)
+  constructor(data: any) {
+    this.#data = data instanceof IDBData ? data as DataInstance : new IDBData(
+      data.parameters,
+      {
+        name: data.name,
+        version: data.version
+      } as any
+    ) as DataInstance;
   }
 
-  /**
-   * 
-   */
-  public get query(): IDBQuery<StoreSchema, Name, StoreNames, Version> {
-    return this.#query;
+  public async ready(
+    callbackfn?: (openRequest: IDBOpenDBRequest) => void
+  ): Promise<IDBOpenDBRequest> {
+    return this.#data.connection.ready(callbackfn);
   }
 
-  /**
-   * 
-   */
-  #query!: IDBQuery<StoreSchema, Name, StoreNames, Version>;
+  public async objectStores<Names extends StoreNames & string>(
+    storeNames: Names[],
+    {
+      ondone,
+      mode,
+      onabort,
+      oncomplete,
+      onerror,
+      options,
+      ontransaction
+    }: IDBQueryTransactionOptions = {}
+  ): Promise<{ [K in Names]: IDBObjectStore }> {
+    return this.#data.objectStores(storeNames, { ondone, mode, onabort, oncomplete, onerror, ontransaction, options });
+  }
 
-  /**
-   * 
-   * @param name 
-   * @param storeNames 
-   * @param store 
-   * @param version 
-   */
-  constructor(
-    name: Name,
-    storeNames: StoreNames | StoreNames[],
-    store?: IDBStoreParameters<StoreNames>,
-    version: Version = 1 as any,
+  public async openStore(
+    name: StoreNames,
+    {
+      mode,
+      onabort,
+      oncomplete,
+      onerror,
+    }: Omit<IDBQueryTransactionOptions, 'ondone' | 'ontransaction' | 'options'> = {}
+  ): Promise<IDBOpenStoreResult> {
+    return this.#data.openStore(name, { mode, onabort, oncomplete, onerror });
+  }
+
+  public async openStores<Names extends StoreNames>(
+    names: Names[],
+    {
+      mode,
+      onabort,
+      oncomplete,
+      onerror,
+      options
+    }: IDBQueryTransactionOptions = {}
+  ): Promise<IDBOpenStoresResult<Names>> {
+    return this.#data.openStores(names, { ondone: undefined, mode, onabort, oncomplete, onerror, options });
+  }
+
+  public async transactStores<Names extends StoreNames & string, R>(
+    names: Names[],
+    {
+      mode,
+      onabort,
+      oncomplete,
+      onerror,
+      options
+    }: IDBQueryTransactionOptions = {},
+    callbackfn: (done: Promise<void>, stores: { [K in Names]: IDBObjectStore }, transaction: IDBTransaction) => Promise<R>
+  ): Promise<R> {
+    return this.#data.transactStores(
+      names,
+      {
+        mode,
+        onabort,
+        oncomplete,
+        onerror,
+        options
+      },
+      callbackfn
+    ) as Promise<R>;
+  }
+
+  public async query(
+    query?: IDBStoreQuery<NoInfer<Schema>, StoreNames>,
+    options: IDBQueryTransactionOptions = {}
   ) {
-    this.#query = new IDBQuery(
-      undefined,
-      { name, storeNames, store, version }
-    );
-  }
-
-  /**
-   * 
-   * @param onOpenSuccess 
-   * @returns 
-   */
-  public onOpenSuccess(
-    onOpenSuccess: (
-      indexeddb: IndexedDB<StoreSchema, Name, StoreNames, Version>,
-      store: IDBStore<StoreSchema, Name, StoreNames, Version>,
-      database: IDBData<Name, StoreNames, Version>,
-      connection: IDBConnection<Name, StoreNames, Version>,
-      openRequest: IDBOpenDBRequest,
-      ev: Event
-    ) => any
-  ): this {
-    this.connection.request.addEventListener(
-      'success',
-      ev => onOpenSuccess(
-        this,
-        this.#query.objectStore,
-        this.#query.objectStore.database,
-        this.connection,
-        this.connection.request,
-        ev
-      ),
-      true
-    );
-    return this;
+    await this.#data.connection.ready();
+    return new IDBQuery<DataInstance, Schema, DBName, StoreNames, Version>(query, options, this.#data);
   }
 }
